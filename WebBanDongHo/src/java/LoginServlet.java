1ec9a3816a144af1f4e0165bd4dfc1f2dc365f92
bach <bachndhe170904@fpt.edu.vn>
2023-05-21 01:33:16 +0700

    commit Login page

---
+++ WebBanDongHo/src/java/LoginServlet.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+import jakarta.servlet.ServletContext;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            //Get data from HTML form  
+            String u = request.getParameter("user");
+            String p = request.getParameter("pass");
+            //Get data from XML
+            ServletContext sc = getServletContext();
+            String user = sc.getInitParameter("username");
+            String pass = sc.getInitParameter("password");
+            if (user.equals(u) && pass.equals(p)) {
+                response.sendRedirect("WelcomeServlet");
+            } else {
+                response.sendRedirect("login.html");
+            }
+        }
+    }
+}
