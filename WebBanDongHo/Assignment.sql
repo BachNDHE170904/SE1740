28228db654f53eaf954c22c4018405964d8c3935
bach <bachndhe170904@fpt.edu.vn>
2023-06-03 16:16:24 +0700

    more products+ fix

--- WebBanDongHo/Assignment.sql
+++ WebBanDongHo/Assignment.sql
@@ -3,14 +3,16 @@ USE WatchDb
 CREATE TABLE Watches (
    id   INT    NOT NULL identity(1,1),
    name NVARCHAR (50)     NOT NULL,
-   price INT	NOT NULL,
+   price float	NOT NULL,
    PRIMARY KEY (id),
 );
 drop table Watches
 SET IDENTITY_INSERT Watches OFF
-insert into Watches(name,price) values ('Inverness - Walnut & Black Leather',124);
-insert into Watches(name,price) values ('Inverness SS - Walnut',148);
-insert into Watches(name,price) values ('Inverness - Walnut & Brown Leather',124);
-insert into Watches(name,price) values ('Inverness - Zebrawood & Black Leather',124);
-insert into Watches(name,price) values ('Inverness - Multi Bamboo & Brown Leather',100);
+insert into Watches(name,price) values ('Inverness - Walnut & Black Leather',124.00);
+insert into Watches(name,price) values ('Inverness SS - Walnut',148.00);
+insert into Watches(name,price) values ('Inverness - Walnut & Brown Leather',124.00);
+insert into Watches(name,price) values ('Inverness - Zebrawood & Black Leather',124.00);
+insert into Watches(name,price) values ('Inverness - Multi Bamboo & Brown Leather',100.00);
+insert into Watches(name,price) values ('Inverness - Multi Bamboo Limited Edition',148.00);
+insert into Watches(name,price) values ('Kylemore - Bamboo Black Leather',86.75);
 select *from Watches
