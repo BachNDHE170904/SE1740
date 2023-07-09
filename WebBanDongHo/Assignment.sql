b8e5def0cf2b195978d5912ca81b3cff1e17afb1
bach <bachndhe170904@fpt.edu.vn>
2023-07-09 20:43:37 +0700

    add watchSpecs db

--- WebBanDongHo/Assignment.sql
+++ WebBanDongHo/Assignment.sql
@@ -31,6 +31,51 @@ CREATE TABLE Orders (
    FOREIGN KEY (username) REFERENCES Accounts(username),
    FOREIGN KEY (watchid) REFERENCES Watches(id),
 );
-select * from Watches
+CREATE TABLE WatchSpecs (
+   id   INT    NOT NULL identity(1,1),
+   bezel NVARCHAR (80)     NOT NULL,
+   movement NVARCHAR (80)     NOT NULL,
+   dial NVARCHAR (80)     NOT NULL,
+   watchCase NVARCHAR (80)     NOT NULL,
+   glass NVARCHAR (80)     NOT NULL,
+   strap NVARCHAR (80)     NOT NULL,
+   PRIMARY KEY (id),
+   FOREIGN KEY (id) REFERENCES Watches(id),
+);
+
+insert into WatchSpecs(bezel,movement,dial,watchCase,glass,strap)values('Walnut','Miyota Japanese Quartz 3 hand with date',
+'Features applied indices, printed seconds track, and custom molded hands',
+'45mm, 50 meter water resistant 316L stainless steel case, and caseback',
+'Hardened Mineral Crystal','quick release leather adjustable strap with memory lock stainless steel clasp');
+insert into WatchSpecs(bezel,movement,dial,watchCase,glass,strap)values('Walnut','Miyota Japanese Quartz 3 hand with date',
+'Features applied indices, printed seconds track, and custom molded hands',
+'45mm, 50 meter water resistant 316L stainless steel case, and caseback',
+'Hardened Mineral Crystal','Stainless steel adjustable strap with stainless steel clasp');
+insert into WatchSpecs(bezel,movement,dial,watchCase,glass,strap)values('Walnut','Miyota Japanese Quartz 3 hand with date',
+'Features applied indices, printed seconds track, and custom molded hands',
+'45mm, 50 meter water resistant 316L stainless steel case, and caseback',
+'Hardened Mineral Crystal','quick release leather adjustable strap with memory lock stainless steel clasp');
+insert into WatchSpecs(bezel,movement,dial,watchCase,glass,strap)values('Zebrawood','Miyota Japanese Quartz 3 hand with date',
+'Features applied indices, printed seconds track, and custom molded hands',
+'45mm, 50 meter water resistant 316L stainless steel case, and caseback',
+'Hardened Mineral Crystal','quick release leather adjustable strap with memory lock stainless steel clasp');
+insert into WatchSpecs(bezel,movement,dial,watchCase,glass,strap)values('Multi Colored Bamboo',
+'Miyota Japanese Quartz 3 hand with date',
+'Features applied indices, printed seconds track, and custom molded hands',
+'45mm, 50 meter water resistant 316L stainless steel case, and caseback',
+'Hardened Mineral Crystal','quick release leather adjustable strap with memory lock stainless steel clasp');
+insert into WatchSpecs(bezel,movement,dial,watchCase,glass,strap)values('Multi Colored Bamboo','Miyota Japanese Quartz 3 hand with date',
+'Features applied indices, printed seconds track, and custom molded hands',
+'45mm, 50 meter water resistant 316L stainless steel case, and caseback',
+'Hardened Mineral Crystal','24 mm wide custom bamboo 3 link bracelet with stainless steel clasp.');
+insert into WatchSpecs(bezel,movement,dial,watchCase,glass,strap)values('Multi Colored Bamboo','Miyota Japanese Quartz 3 hand with date',
+'Features applied indices, printed seconds track, and custom molded hands',
+'45mm, 50 meter water resistant 316L stainless steel case, and caseback',
+'Hardened Mineral Crystal','quick release leather adjustable strap with memory lock stainless steel clasp');
+insert into WatchSpecs(bezel,movement,dial,watchCase,glass,strap)values('Walnut','Miyota Japanese Quartz 3 hand with date',
+'Features applied indices, printed seconds track, and custom molded hands',
+'45mm, 50 meter water resistant 316L stainless steel case, and caseback',
+'Hardened Mineral Crystal','Stainless steel adjustable strap with stainless steel clasp');
+select * from Watches w,WatchSpecs ws where w.id=ws.id 
 select * from Accounts
 select o.id,o.quantity,o.username,o.watchid,w.name,w.price,w.sku from Orders o,Accounts a,Watches w where o.username=a.username and o.watchid=w.id;
